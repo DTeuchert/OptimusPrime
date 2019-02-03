677086eb5483a68f536c0a54702f131b493aeedb
DTeuchert <denny.teuchert@web.de>
2019-02-03 13:12:18 +0100

    Add some prime tests

---
+++ test/OptimusPrime.Server.Tests/Services/PrimeServiceTest.cs
@@ -0,0 +1,40 @@
+using OptimusPrime.Server.Services;
+using Xunit;
+
+namespace OptimusPrime.Server.Tests.Services
+{
+    public class PrimeServiceTest
+    {
+        private readonly IPrimeService _primeService;
+
+        public PrimeServiceTest()
+        {
+            _primeService = new PrimeService();
+        }
+
+        [Fact]
+        public void IsPrime()
+        {
+            /* Arrange */
+            /* Act */
+            var result = _primeService.IsPrime(1);
+
+            /* Assert */
+            Assert.False(result, "1 should not be prime");
+        }
+
+        [Theory]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(5)]
+        public void IsPrime(int number)
+        {
+            /* Arrange */
+            /* Act */
+            var result = _primeService.IsPrime(number);
+
+            /* Assert */
+            Assert.True(result);
+        }
+    }
+}
