b5565d966b18407ac9701750c3d45c7624e8fe27
DTeuchert <denny.teuchert@web.de>
2019-08-15 17:55:23 +0200

    Add health check

---
+++ src/OptimusPrime.Server/Controllers/HealthController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OptimusPrime.Server.Entities;
+using OptimusPrime.Server.Repositories;
+
+namespace OptimusPrime.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public bool Get()
+        {
+            return true;
+        }
+    }
+}
