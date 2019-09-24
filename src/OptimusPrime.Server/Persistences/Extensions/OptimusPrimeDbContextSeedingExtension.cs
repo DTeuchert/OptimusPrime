fc58d63834c07059b5d13ac724d1c2a1364b3fe0
DTeuchert <denny.teuchert@web.de>
2019-09-24 16:08:46 +0200

    Refactor seeding

---
+++ src/OptimusPrime.Server/Persistences/Extensions/OptimusPrimeDbContextSeedingExtension.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using OptimusPrime.Server.Entities;
+
+namespace OptimusPrime.Server.Persistences.Extensions
+{
+    public static class OptimusPrimeDbContextSeedingExtension
+    {
+        public static void ApplySeedings(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(GetPreconfiguredCategorys());
+            modelBuilder.Entity<Transformer>().HasData(GetPreconfiguredTransformers());
+        }
+
+        private static IEnumerable<Category> GetPreconfiguredCategorys()
+        {
+            return new List<Category>()
+            {
+                new Category { Id = 1, Name = "Main" },
+                new Category { Id = 2, Name = "Autobot Cars" },
+                new Category { Id = 3, Name = "Mini-Bots" },
+                new Category { Id = 4, Name = "Dinobots" },
+                new Category { Id = 5, Name = "Aerialbots" },
+                new Category { Id = 6, Name = "Protectobots" },
+                new Category { Id = 7, Name = "Female Autobots" },
+                new Category { Id = 8, Name = "Technobots" },
+                new Category { Id = 9, Name = "Throttlebots" },
+                new Category { Id = 10, Name = "Targetmasters" },
+                new Category { Id = 11, Name = "Headmasters" },
+                new Category { Id = 12, Name = "Clonebots" },
+                new Category { Id = 13, Name = "Junkions" },
+                new Category { Id = 14, Name = "Others" },
+            };
+        }
+        private static IEnumerable<Transformer> GetPreconfiguredTransformers()
+        {
+            return new List<Transformer>()
+            {
+                new Transformer
+                {
+                    Guid = Guid.NewGuid().ToString(),
+                    Name = "Optimus Prime",
+                    Alliance = Alliance.Autobot,
+                    CategoryId = 1
+                },
+                new Transformer
+                {
+                    Guid = Guid.NewGuid().ToString(),
+                    Name = "Bumblebee",
+                    Alliance = Alliance.Autobot,
+                    CategoryId = 1
+                },
+                new Transformer
+                {
+                    Guid = Guid.NewGuid().ToString(),
+                    Name = "Cliffjumper",
+                    Alliance = Alliance.Autobot,
+                    CategoryId = 1
+                },
+                new Transformer
+                {
+                    Guid = Guid.NewGuid().ToString(),
+                    Name = "Megatron",
+                    Alliance = Alliance.Decepticon,
+                    CategoryId = 1
+                }
+            };
+        }
+    }
+}
--- src/OptimusPrime.Server/Persistences/OptimusPrimeDbContext.cs
+++ src/OptimusPrime.Server/Persistences/OptimusPrimeDbContext.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using OptimusPrime.Server.Entities;
 using OptimusPrime.Server.Persistences.Extensions;
 
@@ -18,67 +16,7 @@ namespace OptimusPrime.Server.Persistences
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyAllConfigurations();
-
-            #region Data seeding
-            var categories = new List<(int id, string name)>
-            {
-                (id: 1, name: "Main"),
-                (id: 2, name: "Autobot Cars"),
-                (id: 3, name: "Mini-Bots"),
-                (id: 4, name: "Dinobots"),
-                (id: 5, name: "Aerialbots"),
-                (id: 6, name: "Protectobots"),
-                (id: 7, name: "Female Autobots"),
-                (id: 8, name: "Technobots"),
-                (id: 9, name: "Throttlebots"),
-                (id: 10, name: "Targetmasters"),
-                (id: 11, name: "Headmasters"),
-                (id: 12, name: "Clonebots"),
-                (id: 13, name: "Junkions"),
-                (id: 14, name: "Others"),
-
-
-            };
-            foreach (var (id, name) in categories)
-            {
-                modelBuilder.Entity<Category>().HasData(
-                    new Category { Id = id, Name = name });
-            }
-
-            modelBuilder.Entity<Transformer>().HasData(
-                new Transformer
-                {
-                    Guid = Guid.NewGuid().ToString(),
-                    Name = "Optimus Prime",
-                    Alliance = Alliance.Autobot,
-                    CategoryId = 1
-                });
-            modelBuilder.Entity<Transformer>().HasData(
-                new Transformer
-                {
-                    Guid = Guid.NewGuid().ToString(),
-                    Name = "Bumblebee",
-                    Alliance = Alliance.Autobot,
-                    CategoryId = 1
-                });
-            modelBuilder.Entity<Transformer>().HasData(
-               new Transformer
-               {
-                   Guid = Guid.NewGuid().ToString(),
-                   Name = "Cliffjumper",
-                   Alliance = Alliance.Autobot,
-                   CategoryId = 1
-               });
-
-            modelBuilder.Entity<Transformer>().HasData(
-               new Transformer
-               {
-                   Guid = Guid.NewGuid().ToString(),
-                   Name = "Megatron",
-                   Alliance = Alliance.Decepticon,
-                   CategoryId = 1
-               });
-            #endregion
+            modelBuilder.ApplySeedings();
         }
     }
 }
